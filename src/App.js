d38dc7defee9ce5352d07039a60532dc3cb30ece
nikhil-eashy <nikhileashy74@gmail.com>
2023-03-21 02:04:21 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Read from './components/Read';
 
 
 function App() {
   return (
     <div className="App">
-
+<Read></Read>
     </div>
   );
 }
---
+++ src/components/Read.jsx
@@ -0,0 +1,57 @@
+import { Typography, Paper, styled, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { tableCellClasses } from '@mui/material/TableCell';
+import { blue, blueGrey } from '@mui/material/colors';
+import axios from 'axios';
+
+
+const Read = () => {
+    const color = blue[400];
+    const color2 = blueGrey[900];
+    const StyledTableCell = styled(TableCell)(({ theme }) => ({
+        [`&.${tableCellClasses.head}`]: {
+            backgroundColor: color2,
+            color: color,
+        },
+        [`&.${tableCellClasses.body}`]: {
+            fontSize: 14,
+        },
+    }));
+    var [students, setstud] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:3005/students")
+            .then(response => {
+                console.log(response.data)
+                setstud(students = response.data)
+            })
+            .catch(err => console.log(err))
+    })
+    return (
+        <div>
+            <br></br>
+            <Typography variant='h3'>bbndfjbnkdj</Typography>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <StyledTableCell>Name</StyledTableCell>
+                            <StyledTableCell>Age</StyledTableCell>
+                            <StyledTableCell>Place</StyledTableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {students.map((value, index) => {
+                            return <TableRow>
+                                <TableCell>{value.id}</TableCell>
+                                <TableCell>{value.name}</TableCell>
+                                <TableCell>{value.grade}</TableCell>
+                            </TableRow>
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+}
+
+export default Read
