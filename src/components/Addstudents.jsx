83689d36a714aba9c60bb884c4c2f35d4b700fa3
nikhil-eashy <nikhileashy74@gmail.com>
2023-03-21 22:20:19 -0700

    yo

--- src/components/Addstudents.jsx
+++ src/components/Addstudents.jsx
@@ -1,16 +1,28 @@
 import { Button, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Addstudents = () => {
+    var [students,setstudents]= useState({
+        id:"",
+        name:"",
+        grade:''
+    })
+
+    const handler = (e) => {
+        const{name,value} = e.target
+        setstudents({... students,[name]:value})
+        console.log(students)
+    }
   return (
     <div>
       <Typography variant='h3'>Add Students</Typography><br></br>
-      <TextField variant='outlined' label="ID"></TextField><br></br><br></br>
+      <TextField variant='outlined' label="ID" onChange={handler}> </TextField><br></br><br></br>
       <TextField variant='outlined' label="Name">name</TextField><br></br><br></br>
       <TextField variant='outlined' label="Grade">grade</TextField><br></br><br></br>
       <Button variant='contained'>SUBMIT</Button>
     </div>
   )
 }
+
 
 export default Addstudents
